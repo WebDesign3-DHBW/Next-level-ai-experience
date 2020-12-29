0ccebad8b2590b74634ec6d6d9f9b3d528c474a1
OleMantei <ole.mantei@gmail.com>
2020-12-29 15:59:00 +0100

    add section2

--- src/sections/section2/Section2.jsx
+++ src/sections/section2/Section2.jsx
@@ -1,7 +1,35 @@
+import { IconContext } from 'react-icons';
+import { MdFormatShapes } from 'react-icons/md';
+
 function Section2() {
   return (
     <>
-      <p>Section 2</p>
+      <div className="centerbox">
+        <div className="content">
+          <h2 className="heroH2 mb-5">Die Studie</h2>
+          <p>
+            Die Daten des Berichts “Einsatz von Künstlicher Intelligenz in der
+            Deutschen Wirtschaft” des <b>Bundesministerium für Wirtschaft und
+            Energie aus dem Jahr 2019</b> basieren auf einer Sonderauswertung der
+            Deutschen Innovationserhebung des Jahres 2019 sowie einer
+            Zusatzbefragung von KI einsetzenden Unternehmen.
+          </p>
+          <div className="d-flex">
+            <div className="d-flex mr-2 mt-1">
+              <IconContext.Provider value={{ className: 'iconGrey' }}>
+                <MdFormatShapes />
+              </IconContext.Provider>
+            </div>
+            <div className="d-flex">
+              <p>
+                Grafische Aufbereitung der Studie
+                <br /><b> DHBW Mosbach, Onlinemedien </b><br /> Nils Eisenhauer, Felix
+                Keller, Natascha Forster, Ole Mantei
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
