1b9ee44ff4bda1f0a3c772f5aa02ce1429b73665
eisenils <mail.eisenils@gmail.com>
2020-12-29 11:14:33 +0100

    Clean up 🚮

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import Hero from "./components/Hero";
-import BarChart from "./components/Chart1";
-import LineChart from "./components/LineChart";
-import RadarChart from "./components/RadarChart1";
 import "./sass/main.scss";
-import DoughnutChart from "./components/Doughnut";
 
 function App() {
   return (
     <div className='view'>
-      <section className='Section1'>
-        <Hero />
+      <section>
+        <p>Section 1</p>
       </section>
-      <section style={{ backgroundColor: "#0D1534" }}>
-        <DoughnutChart />
+      <section>
+        <p>Section 2</p>
       </section>
-      <section style={{ backgroundColor: "white" }}>
-        <BarChart />
+      <section>
+        <p>Section 3</p>
       </section>
-      <section style={{ backgroundColor: "white" }}>
-        <RadarChart />
+      <section>
+        <p>Section 4</p>
       </section>
-      <section style={{ backgroundColor: "#0D1534" }}>
-        <LineChart />
+      <section>
+        <p>Section 5</p>
       </section>
     </div>
   );
