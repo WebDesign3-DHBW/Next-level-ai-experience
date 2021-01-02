f8860f3aaf4aea7ed59db9b78be6255dc2e65a2b
NataschaForster <forsternatascha@gmail.com>
2021-01-02 16:24:48 +0100

    first double donut thingy

--- src/components/Section4.jsx
+++ src/components/Section4.jsx
@@ -2,71 +2,84 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 
 function Section4() {
-// Create chart instance
-let chart = am4core.create("chartdiv", am4charts.XYChart);
+  am4core.ready(function() {
 
-// Add data
-chart.data = [{
-  "country": "Lithuania",
-  "litres": 501.9,
-  "units": 250
-}, {
-  "country": "Czech Republic",
-  "litres": 301.9,
-  "units": 222
-}, {
-  "country": "Ireland",
-  "litres": 201.1,
-  "units": 170
-}, {
-  "country": "Germany",
-  "litres": 165.8,
-  "units": 122
-}, {
-  "country": "Australia",
-  "litres": 139.9,
-  "units": 99
-}, {
-  "country": "Austria",
-  "litres": 128.3,
-  "units": 85
-}, {
-  "country": "UK",
-  "litres": 99,
-  "units": 93
-}, {
-  "country": "Belgium",
-  "litres": 60,
-  "units": 50
-}, {
-  "country": "The Netherlands",
-  "litres": 50,
-  "units": 42
-}];
-
-// Create axes
-let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "country";
-categoryAxis.title.text = "Countries";
-
-let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.title.text = "Litres sold (M)";
-
-// Create series
-let series = chart.series.push(new am4charts.ColumnSeries());
-series.dataFields.valueY = "litres";
-series.dataFields.categoryX = "country";
-series.name = "Sales";
-series.columns.template.tooltipText = "Series: {name}\nCategory: {categoryX}\nValue: {valueY}";
-series.columns.template.fill = am4core.color("#104547"); // fill
-
-var series2 = chart.series.push(new am4charts.LineSeries());
-series2.name = "Units";
-series2.stroke = am4core.color("#CDA2AB");
-series2.strokeWidth = 3;
-series2.dataFields.valueY = "units";
-series2.dataFields.categoryX = "country";
-
+    // Themes begin
+    //am4core.useTheme(am4themes_animate);
+    // Themes end
+    
+    // Create chart instance
+    var chart = am4core.create("chartdiv", am4charts.PieChart);
+    
+    // Let's cut a hole in our Pie chart the size of 40% the radius
+    chart.innerRadius = am4core.percent(40);
+    
+    // Add data
+    chart.data = [{
+      "country": "Lithuania",
+      "litres": 501.9,
+      "bottles": 1500
+    }, {
+      "country": "Czech Republic",
+      "litres": 301.9,
+      "bottles": 990
+    }, {
+      "country": "Ireland",
+      "litres": 201.1,
+      "bottles": 785
+    }, {
+      "country": "Germany",
+      "litres": 165.8,
+      "bottles": 255
+    }, {
+      "country": "Australia",
+      "litres": 139.9,
+      "bottles": 452
+    }, {
+      "country": "Austria",
+      "litres": 128.3,
+      "bottles": 332
+    }, {
+      "country": "UK",
+      "litres": 99,
+      "bottles": 150
+    }, {
+      "country": "Belgium",
+      "litres": 60,
+      "bottles": 178
+    }, {
+      "country": "The Netherlands",
+      "litres": 50,
+      "bottles": 50
+    }];
+    
+    // Add and configure Series
+    var pieSeries = chart.series.push(new am4charts.PieSeries());
+    pieSeries.dataFields.value = "litres";
+    pieSeries.dataFields.category = "country";
+    pieSeries.slices.template.stroke = am4core.color("#fff");
+    pieSeries.slices.template.strokeWidth = 2;
+    pieSeries.slices.template.strokeOpacity = 1;
+    
+    // Disabling labels and ticks on inner circle
+    pieSeries.labels.template.disabled = true;
+    pieSeries.ticks.template.disabled = true;
+    
+    // Disable sliding out of slices
+    pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+    pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
+    
+    // Add second series
+    var pieSeries2 = chart.series.push(new am4charts.PieSeries());
+    pieSeries2.dataFields.value = "bottles";
+    pieSeries2.dataFields.category = "country";
+    pieSeries2.slices.template.stroke = am4core.color("#fff");
+    pieSeries2.slices.template.strokeWidth = 2;
+    pieSeries2.slices.template.strokeOpacity = 1;
+    pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+    pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
+    
+    }); // end am4core.ready()
 return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
 }
 
