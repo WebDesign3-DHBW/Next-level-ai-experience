4b02aeb0b8a11f1d15caf70675d587dfa2207757
NataschaForster <forsternatascha@gmail.com>
2021-01-04 12:00:12 +0100

    first setup

--- src/sections/section7/Section7.jsx
+++ src/sections/section7/Section7.jsx
@@ -1,7 +1,26 @@
+import Chart1 from "./Chart1";
+import Chart2 from "./Chart2";
+import Chart3 from "./Chart3";
+import Chart4 from "./Chart4";
+import Chart5 from "./Chart5";
+
 function Section7() {
   return (
     <>
-      <p>Section 7</p>
+      <h1>Anwendungsgebiete</h1>
+      <div className='row'>
+        <div className='col-8 '>
+        < p>Künstliche Intelligenz wird am häufigsten im Bereich von Produkten und Dienstleistungen (60 %) der KI
+          einsetzenden Unternehmen verwendet. Knapp dahinter wird KI für die Automatisierung von Prozessen genutzt (56 %). Eine Analyse mit KI Daten setzen 34 % der Unternehmen ein und zu 22 % wird künstliche Intelligenz zur Kommunikation mit Kunden verwendet.</p>
+        </div>
+      </div>
+      <div className="chartcontainer">
+          <Chart1 /> 
+          <Chart2 />
+          <Chart3 />
+          <Chart4 />
+          <Chart5 />
+      </div> 
     </>
   );
 }
