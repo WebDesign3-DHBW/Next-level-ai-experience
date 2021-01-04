f5827eed3e6d9c7ba1785bc657e11222797f8c20
Nils <46008838+eisenils@users.noreply.github.com>
2021-01-04 17:04:00 +0100

    Update src/sections/section7/Section7.jsx

--- src/sections/section7/Section7.jsx
+++ src/sections/section7/Section7.jsx
@@ -10,7 +10,7 @@ function Section7() {
       <h1>Anwendungsgebiete</h1>
       <div className='row'>
         <div className='col-8 '>
-        < p>Künstliche Intelligenz wird am häufigsten im Bereich von Produkten und Dienstleistungen (60 %) der KI
+        <p>Künstliche Intelligenz wird am häufigsten im Bereich von Produkten und Dienstleistungen (60 %) der KI
           einsetzenden Unternehmen verwendet. Knapp dahinter wird KI für die Automatisierung von Prozessen genutzt (56 %). Eine Analyse mit KI Daten setzen 34 % der Unternehmen ein und zu 22 % wird künstliche Intelligenz zur Kommunikation mit Kunden verwendet.</p>
         </div>
       </div>
