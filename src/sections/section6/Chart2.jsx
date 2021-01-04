f49228cba2508bacfc96b320e0960b4879a3051b
NataschaForster <forsternatascha@gmail.com>
2021-01-04 10:40:07 +0100

    double donut layout

---
+++ src/sections/section6/Chart2.jsx
@@ -0,0 +1,67 @@
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import am4themes_wdTheme from "../../theme";
+
+am4core.useTheme(am4themes_animated);
+am4core.useTheme(am4themes_wdTheme);
+
+function Chart2() {
+    am4core.ready(function() {
+      
+      // Create chart instance
+      var chart = am4core.create("chartdiv_sec6_2", am4charts.PieChart);
+      
+      // Let's cut a hole in our Pie chart the size of 40% the radius
+      chart.innerRadius = am4core.percent(40);
+      
+      // Add data
+      chart.data = [{
+        "Nutzen": "Nutzen Bild- und Tonerkennung",
+        "IKT": 47,
+        "Gesamtwirtschaft": 49
+
+      }, {
+        "Nutzen": "Nutzen es nicht",
+        "IKT": 53,
+        "Gesamtwirtschaft": 51
+      }
+
+    ];
+      
+      // Add and configure Series
+      var pieSeries = chart.series.push(new am4charts.PieSeries());
+      pieSeries.dataFields.value = "IKT";
+      pieSeries.dataFields.category = "Nutzen";
+      pieSeries.slices.template.stroke = am4core.color("#fff");
+      pieSeries.slices.template.strokeWidth = 2;
+      pieSeries.slices.template.strokeOpacity = 1;
+      
+      // Disabling labels and ticks on inner circle
+      pieSeries.labels.template.disabled = true;
+      pieSeries.ticks.template.disabled = true;
+      
+      // Disable sliding out of slices
+      pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
+      
+      // Add second series
+      var pieSeries2 = chart.series.push(new am4charts.PieSeries());
+      pieSeries2.dataFields.value = "Gesamtwirtschaft";
+      pieSeries2.dataFields.category = "Nutzen";
+      pieSeries2.slices.template.stroke = am4core.color("#fff");
+      pieSeries2.slices.template.strokeWidth = 2;
+      pieSeries2.slices.template.strokeOpacity = 1;
+      pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
+      
+      let title = chart.titles.create();
+      title.text = "Bild- und Tonerkennung";
+      title.marginBottom = "5";
+      title.marginTop = "20";
+
+      }); // end am4core.ready()
+  return <div id="chartdiv_sec6_2" style={{ width: "100%", height: "300px" }}></div>;
+  }
+  
+  export default Chart2;
--- src/sections/section6/Section6.jsx
+++ src/sections/section6/Section6.jsx
@@ -1,7 +1,25 @@
+import Chart1 from "./Chart1";
+import Chart2 from "./Chart2";
+import Chart3 from "./Chart3";
+import Chart4 from "./Chart4";
+
 function Section6() {
   return (
     <>
       <p>Section 6</p>
+      <h1>KI Verfahren</h1>
+      <div className='row'>
+        <div className='col-8 '>
+        < p>Am weitesten verbreitet war im Jahr 2019 in der deutschen Wirtschaft maschinelles Lernen und Beweisen als eingesetztes Verfahren mit 55 %.  Zur Erkennung von Bild und Ton setzen 49 % der Unternehmen auf KI. “Wissensbasierte
+        Systeme" werden beinahe eben so häufig genutzt. Sprach- und Textverstehen sind mit 30 % repräsentiert.</p>
+        </div>
+      </div>
+      <div className="container d-flex">
+        <Chart1 className="mx-auto" /> 
+        <Chart2 />
+        <Chart3 />
+        <Chart4 />
+      </div>  
     </>
   );
 }
