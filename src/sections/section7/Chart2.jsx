5648c42975ce9b10c252bc1825388da92b08766b
NataschaForster <forsternatascha@gmail.com>
2021-01-04 15:38:22 +0100

    color styling

--- src/sections/section7/Chart2.jsx
+++ src/sections/section7/Chart2.jsx
@@ -34,8 +34,12 @@ function Chart2() {
       pieSeries.dataFields.value = "IKT";
       pieSeries.dataFields.category = "Nutzen";
       pieSeries.slices.template.stroke = am4core.color("#fff");
-      pieSeries.slices.template.strokeWidth = 2;
-      pieSeries.slices.template.strokeOpacity = 1;
+      pieSeries.slices.template.strokeWidth = 0;
+      pieSeries.slices.template.tooltipText = "IKT: {IKT}";
+      pieSeries.radius = am4core.percent(43);
+      pieSeries.colors.list = [
+        am4core.color("#EDE750"),
+        am4core.color("rgba(0, 77, 136, 0.3)")]
       
       // Disabling labels and ticks on inner circle
       pieSeries.labels.template.disabled = true;
@@ -50,12 +54,15 @@ function Chart2() {
       pieSeries2.dataFields.value = "Gesamtwirtschaft";
       pieSeries2.dataFields.category = "Nutzen";
       pieSeries2.slices.template.stroke = am4core.color("#fff");
-      pieSeries2.slices.template.strokeWidth = 2;
-      pieSeries2.slices.template.strokeOpacity = 1;
+      pieSeries2.slices.template.strokeWidth = 0;
       pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
       pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
       pieSeries2.labels.template.disabled = true;
       pieSeries2.ticks.template.disabled = true;
+      pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
+      pieSeries2.colors.list = [
+        am4core.color("#4F4FFE"),
+        am4core.color("rgba(0, 77, 136, 0.3)")]
       
       let label = chart.createChild(am4core.Label);
       label.text = "Automatisierung von Prozessen";
