97322733f45da6d23e8c9ed2e259420d18a5f250
NataschaForster <forsternatascha@gmail.com>
2021-01-04 15:33:19 +0100

    color changes

--- src/sections/section6/Chart1.jsx
+++ src/sections/section6/Chart1.jsx
@@ -1,10 +1,10 @@
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import am4themes_wdTheme from "../../theme";
 
 am4core.useTheme(am4themes_animated);
-am4core.useTheme(am4themes_wdTheme);
+
+
 
 function Chart1() {
     am4core.ready(function() {
@@ -12,6 +12,7 @@ function Chart1() {
       // Create chart instance
       var chart = am4core.create("chartdiv_sec6", am4charts.PieChart);
       
+
       // Let's cut a hole in our Pie chart the size of 40% the radius
       chart.innerRadius = am4core.percent(40);
       
@@ -24,7 +25,8 @@ function Chart1() {
       }, {
         "Nutzen": "Nutzen es nicht",
         "IKT": 41,
-        "Gesamtwirtschaft": 45
+        "Gesamtwirtschaft": 45,
+        "color": am4core.color("#cc0000")
       }
 
     ];
@@ -34,9 +36,12 @@ function Chart1() {
       pieSeries.dataFields.value = "IKT";
       pieSeries.dataFields.category = "Nutzen";
       pieSeries.slices.template.stroke = am4core.color("#fff");
-      pieSeries.slices.template.strokeWidth = 2;
-      pieSeries.slices.template.strokeOpacity = 1;
+      pieSeries.slices.template.strokeWidth = 0;
       pieSeries.slices.template.tooltipText = "IKT: {IKT}";
+      pieSeries.radius = am4core.percent(43);
+      pieSeries.colors.list = [
+        am4core.color("#EDE750"),
+        am4core.color("rgba(0, 77, 136, 0.3)")]
       
       // Disabling labels and ticks on inner circle
       pieSeries.labels.template.disabled = true;
@@ -45,19 +50,21 @@ function Chart1() {
       // Disable sliding out of slices
       pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
       pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
-      
+
       // Add second series
       var pieSeries2 = chart.series.push(new am4charts.PieSeries());
       pieSeries2.dataFields.value = "Gesamtwirtschaft";
       pieSeries2.dataFields.category = "Nutzen";
       pieSeries2.slices.template.stroke = am4core.color("#fff");
-      pieSeries2.slices.template.strokeWidth = 2;
-      pieSeries2.slices.template.strokeOpacity = 1;
+      pieSeries2.slices.template.strokeWidth = 0;
       pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
       pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
       pieSeries2.labels.template.disabled = true;
       pieSeries2.ticks.template.disabled = true;
       pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
+      pieSeries2.colors.list = [
+        am4core.color("#4F4FFE"),
+        am4core.color("rgba(0, 77, 136, 0.3)")]
 
       let label = chart.createChild(am4core.Label);
       label.text = "Maschinelles Lernen und Beweisen";
