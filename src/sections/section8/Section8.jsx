3ea452a2233fc09307e6db20fa56b5821c597a6b
Felix Keller <felixkeller98@gmail.com>
2021-01-04 14:20:06 +0100

    initial setup

--- src/sections/section8/Section8.jsx
+++ src/sections/section8/Section8.jsx
@@ -1,9 +1,23 @@
+import Chart from "../section8/Chart";
+
 function Section8() {
   return (
-    <>
-      <p>Section 8</p>
-    </>
+    <div className="container d-flex align-items-center" style={{ height: "100vh" }}>
+      <div className="row">
+        <div className="col-8 mx-auto">
+          <h2 className="mb-2 text-center">Bedeutung des Einsatzes von KI</h2>
+          <p>
+            Der Einsatz von KI wird von den meisten Unternehmen, die KI einsetzen (65 %), als
+            wichtig für die Geschäftigkeit eingestuft. Für 12 % der KI einsetzenden Unternehmen ist
+            KI essenziell für ihre Geschäftstätigkeit, d.h. ohne KI könnten die entsprechenden
+            Aktivitäten gar nicht ausgeführt werden.
+          </p>
+        </div>
+        <div className="col-12 mt-4">
+          <Chart />
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default Section8;
