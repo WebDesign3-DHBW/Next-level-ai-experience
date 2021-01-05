93492e8d5961930e650d560fa1ea631fc16a85cb
eisenils <mail.eisenils@gmail.com>
2021-01-05 15:29:04 +0100

    First buggy implement of lazy loading (veery lazy) 🐢

--- src/App.js
+++ src/App.js
@@ -9,51 +9,57 @@ import Section7 from "./sections/section7/Section7";
 import Section8 from "./sections/section8/Section8";
 import Section9 from "./sections/section9/Section9";
 import Footer from "./sections/footer/Footer";
+import * as am4core from "@amcharts/amcharts4/core";
+
+// Enable queuing
+// am4core.options.queue = true;
+// am4core.options.onlyShowOnViewport = true;
+// am4core.options.viewportTarget = document.getElementById("charts");
 
 function App() {
   return (
-    <div className="view">
-      <div className="bg-section1">
+    <div className='view'>
+      <div className='bg-section1'>
         <section>
           <Section1 />
         </section>
       </div>
-      <div className="bg-section2">
+      <div className='bg-section2'>
         <section>
           <Section2 />
         </section>
       </div>
-      <div className="bg-A">
+      <div className='bg-A'>
         <section>
           <Section3 />
         </section>
       </div>
-      <div className="bg-B">
+      <div className='bg-B'>
         <section>
           <Section4 />
         </section>
       </div>
-      <div className="bg-A">
+      <div className='bg-A'>
         <section>
           <Section5 />
         </section>
       </div>
-      <div className="bg-B">
+      <div className='bg-B'>
         <section>
           <Section6 />
         </section>
       </div>
-      <div className="bg-A">
+      <div className='bg-A'>
         <section>
           <Section7 />
         </section>
       </div>
-      <div className="bg-B">
+      <div className='bg-B'>
         <section>
           <Section8 />
         </section>
       </div>
-      <div className="bg-A">
+      <div className='bg-A'>
         <section>
           <Section9 />
         </section>
