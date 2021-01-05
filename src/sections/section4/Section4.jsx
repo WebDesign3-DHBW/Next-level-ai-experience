4d730ae99e0fe6cbfcef3b8887bdb900f5d34d62
OleMantei <ole.mantei@gmail.com>
2021-01-05 08:27:11 +0100

    add styling h2 box

--- src/sections/section4/Section4.jsx
+++ src/sections/section4/Section4.jsx
@@ -2,7 +2,7 @@ function Section4() {
   return (
     <>
       <div className="pt-5">
-        <div className="h2-box mb-5">
+        <div className="h2r-box mb-5">
           <h2>Indikatoren zur Verbreitung des Einsatzes von KI</h2>
         </div>
       </div>
