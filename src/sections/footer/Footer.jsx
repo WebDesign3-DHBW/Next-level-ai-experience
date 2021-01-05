467adae380e292b358326ece6c96104eea4b9cd6
eisenils <mail.eisenils@gmail.com>
2021-01-05 11:01:42 +0100

    First implement of Footer 👣

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,7 +1,46 @@
 function Footer() {
   return (
     <>
-      <p>Footer</p>
+      <div className='d-flex justify-content-center flex-column' style={{ height: "100vh" }}>
+        <div className='pt-5'>
+          <div className='h2-box mb-5'>
+            <h2>Impressum</h2>
+          </div>
+        </div>
+        <p>
+          DHBW Campus Mosbach, Studiengang Onlinemedien <br />
+          Lohrtalweg 10
+          <br />
+          74821 Mosbach
+          <br />
+          <a href='https://www.mosbach.dhbw.de/' className='text-decoration-none'>
+            www.mosbach.dhbw.de
+          </a>
+        </p>
+        <p>
+          Web Design 3, 29.01.2021
+          <br />
+          Dozent: Dipl.-Des. Lothar B. Blum
+          <br />
+          Kurs: ON18A
+          <br />
+          Team: Nils Eisenhauer, Natascha Forster, Felix Keller, Ole Mantei
+        </p>
+        <p>
+          Datengrundlage
+          <br /> Einsatz von Künstlicher Intelligenz in der Deutschen Wirtschaft <br />
+          Stand der KI-Nutzung im Jahr 2019 <br />
+          Bundesministerium für Wirtschaft und Energie (BMWi)
+          <br />
+          Onlinepublikation{" "}
+          <a
+            className='text-decoration-none'
+            href='https://www.bmwi.de/Redaktion/DE/Publikationen/Wirtschaft/einsatz-von-ki-deutsche-wirtschaft.html'>
+            hier
+          </a>{" "}
+          verfügbar
+        </p>
+      </div>
     </>
   );
 }
