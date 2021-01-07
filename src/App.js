8db3028903937853cb3379d02a8da5ff92cb4ff0
OleMantei <ole.mantei@gmail.com>
2021-01-07 15:22:26 +0100

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./sass/main.scss";
+import ReactFullpage from "@fullpage/react-fullpage";
 import Section1 from "./sections/section1/Section1";
 import Section2 from "./sections/section2/Section2";
 import Section3 from "./sections/section3/Section3";
@@ -12,58 +13,101 @@ import Footer from "./sections/footer/Footer";
 
 function App() {
   return (
-    <div className='view'>
-      <div className='bg-section1'>
-        <section>
-          <Section1 />
-        </section>
-      </div>
-      <div className='bg-section2'>
-        <section>
-          <Section2 />
-        </section>
-      </div>
-      <div className='bg-A'>
-        <section>
-          <Section3 />
-        </section>
-      </div>
-      <div className='bg-B'>
-        <section>
-          <Section4 />
-        </section>
-      </div>
-      <div className='bg-A'>
-        <section>
-          <Section5 />
-        </section>
-      </div>
-      <div className='bg-B'>
-        <section>
-          <Section6 />
-        </section>
-      </div>
-      <div className='bg-A'>
-        <section>
-          <Section7 />
-        </section>
-      </div>
-      <div className='bg-B'>
-        <section>
-          <Section8 />
-        </section>
-      </div>
-      <div className='bg-A'>
-        <section>
-          <Section9 />
-        </section>
-      </div>
-      <div className='bg-footer'>
-        <section>
-          <Footer />
-        </section>
-      </div>
-    </div>
+    <ReactFullpage
+      scrollingSpeed={1000}
+      navigation={true}
+      navigationPosition={"right"}
+      navigationTooltips={[
+        "Start",
+        "Studie",
+        "Bedeutung",
+        "Indikatoren",
+        "Einsatz",
+        "Verfahren",
+        "Anwendungsgebiete",
+        "Umsatz",
+        "Ausgaben",
+        "Impressum",
+      ]}
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="view">
+              <div className="section">
+                <div className="bg-section1">
+                  <section>
+                    <Section1 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-section2">
+                  <section>
+                    <Section2 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-A">
+                  <section>
+                    <Section3 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-B">
+                  <section>
+                    <Section4 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-A">
+                  <section>
+                    <Section5 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-B">
+                  <section>
+                    <Section6 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-A">
+                  <section>
+                    <Section7 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-B">
+                  <section>
+                    <Section8 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div className="bg-A">
+                  <section>
+                    <Section9 />
+                  </section>
+                </div>
+              </div>
+              <div className="section">
+                <div>
+                  <section>
+                    <Footer />
+                  </section>
+                </div>
+              </div>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 }
 
