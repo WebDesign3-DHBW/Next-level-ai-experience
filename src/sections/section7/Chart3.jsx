f96ad1457979a1ace8b9f8deede9096596a67caa
eisenils <mail.eisenils@gmail.com>
2021-01-07 16:09:12 +0100

    Section 6 & 7 are now lazy

--- src/sections/section7/Chart3.jsx
+++ src/sections/section7/Chart3.jsx
@@ -2,54 +2,57 @@ import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4themes_wdTheme from "../../theme";
+import handleViewport from "react-in-viewport";
+import { useLayoutEffect, useRef } from "react";
 
 am4core.useTheme(am4themes_animated);
 am4core.useTheme(am4themes_wdTheme);
 
-function Chart3() {
-    am4core.ready(function() {
-      
+function Chart3(props) {
+  const chart = useRef(null);
+  const { inViewport, forwardedRef } = props;
+
+  useLayoutEffect(() => {
+    if (inViewport) {
       // Create chart instance
-      var chart = am4core.create("chartdiv_sec7_3", am4charts.PieChart);
-      
+      var x = am4core.create("chartdiv_sec7_3", am4charts.PieChart);
+
       // Let's cut a hole in our Pie chart the size of 40% the radius
-      chart.innerRadius = am4core.percent(40);
-      
+      x.innerRadius = am4core.percent(40);
+
       // Add data
-      chart.data = [{
-        "Nutzen": "Nutzen KI Anwendungen für Datenanalyse",
-        "IKT": 35,
-        "Gesamtwirtschaft": 34
+      x.data = [
+        {
+          Nutzen: "Nutzen KI Anwendungen für Datenanalyse",
+          IKT: 35,
+          Gesamtwirtschaft: 34,
+        },
+        {
+          Nutzen: "Nutzen es nicht",
+          IKT: 65,
+          Gesamtwirtschaft: 66,
+        },
+      ];
 
-      }, {
-        "Nutzen": "Nutzen es nicht",
-        "IKT": 65,
-        "Gesamtwirtschaft": 66
-      }
-
-    ];
-      
       // Add and configure Series
-      var pieSeries = chart.series.push(new am4charts.PieSeries());
+      var pieSeries = x.series.push(new am4charts.PieSeries());
       pieSeries.dataFields.value = "IKT";
       pieSeries.dataFields.category = "Nutzen";
       pieSeries.slices.template.strokeWidth = 0;
       pieSeries.slices.template.tooltipText = "IKT: {IKT}";
       pieSeries.radius = am4core.percent(43);
-      pieSeries.colors.list = [
-        am4core.color("#EDE750"),
-        am4core.color("rgba(0, 77, 136, 0.3)")]
-      
+      pieSeries.colors.list = [am4core.color("#EDE750"), am4core.color("rgba(0, 77, 136, 0.3)")];
+
       // Disabling labels and ticks on inner circle
       pieSeries.labels.template.disabled = true;
       pieSeries.ticks.template.disabled = true;
-      
+
       // Disable sliding out of slices
       pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
       pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
-      
+
       // Add second series
-      var pieSeries2 = chart.series.push(new am4charts.PieSeries());
+      var pieSeries2 = x.series.push(new am4charts.PieSeries());
       pieSeries2.dataFields.value = "Gesamtwirtschaft";
       pieSeries2.dataFields.category = "Nutzen";
       pieSeries2.slices.template.strokeWidth = 0;
@@ -58,11 +61,9 @@ function Chart3() {
       pieSeries2.labels.template.disabled = true;
       pieSeries2.ticks.template.disabled = true;
       pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
-      pieSeries2.colors.list = [
-        am4core.color("#4F4FFE"),
-        am4core.color("rgba(0, 77, 136, 0.3)")]
-      
-      let label = chart.createChild(am4core.Label);
+      pieSeries2.colors.list = [am4core.color("#4F4FFE"), am4core.color("rgba(0, 77, 136, 0.3)")];
+
+      let label = x.createChild(am4core.Label);
       label.text = "KI Anwendungen für Datenanalyse";
       label.align = "center";
 
@@ -74,10 +75,13 @@ function Chart3() {
       image.horizontalCenter = "middle";
       image.verticalCenter = "middle";
 
-      chart.logo.disabled = "true";
+      x.logo.disabled = true;
+      chart.current = x;
+    }
+  }, [inViewport]);
+  return (
+    <div id='chartdiv_sec7_3' style={{ width: "100%", height: "300px" }} ref={forwardedRef}></div>
+  );
+}
 
-      }); // end am4core.ready()
-  return <div id="chartdiv_sec7_3" style={{ width: "100%", height: "300px" }}></div>;
-  }
-  
-  export default Chart3;
+export default handleViewport(Chart3);
