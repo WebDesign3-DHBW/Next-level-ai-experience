f22cf0f251b44a8fb3a23e5a75188adcc32244e6
eisenils <mail.eisenils@gmail.com>
2021-01-07 16:09:12 +0100

    Section 6 & 7 are now lazy

--- src/sections/section6/Chart1.jsx
+++ src/sections/section6/Chart1.jsx
@@ -1,75 +1,84 @@
+import { useLayoutEffect, useRef } from "react";
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { buchHref } from "../../svg/svgHref";
+import handleViewport from "react-in-viewport";
 
 am4core.useTheme(am4themes_animated);
 
-function Chart1() {
-  am4core.ready(function () {
-    // Create chart instance
-    var chart = am4core.create("chartdiv_sec6", am4charts.PieChart);
+function Chart1(props) {
+  const chart = useRef(null);
+  const { inViewport, forwardedRef } = props;
 
-    // Let's cut a hole in our Pie chart the size of 40% the radius
-    chart.innerRadius = am4core.percent(40);
+  useLayoutEffect(() => {
+    if (inViewport) {
+      var x = am4core.create("chartdiv_sec6", am4charts.PieChart);
 
-    // Add data
-    chart.data = [
-      {
-        Nutzen: "Nutzen Maschinelles Lernen und Beweisen",
-        IKT: 59,
-        Gesamtwirtschaft: 55,
-      },
-      {
-        Nutzen: "Nutzen es nicht",
-        IKT: 41,
-        Gesamtwirtschaft: 45,
-      },
-    ];
+      // Let's cut a hole in our Pie chart the size of 40% the radius
+      x.innerRadius = am4core.percent(40);
 
-    // Add and configure Series
-    var pieSeries = chart.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "IKT";
-    pieSeries.dataFields.category = "Nutzen";
-    pieSeries.slices.template.strokeWidth = 0;
-    pieSeries.slices.template.tooltipText = "IKT: {IKT}";
-    pieSeries.radius = am4core.percent(43);
-    pieSeries.colors.list = [am4core.color("#EDE750"), am4core.color("rgba(0, 77, 136, 0.3)")];
+      // Add data
+      x.data = [
+        {
+          Nutzen: "Nutzen Maschinelles Lernen und Beweisen",
+          IKT: 59,
+          Gesamtwirtschaft: 55,
+        },
+        {
+          Nutzen: "Nutzen es nicht",
+          IKT: 41,
+          Gesamtwirtschaft: 45,
+        },
+      ];
 
-    // Disabling labels and ticks on inner circle
-    pieSeries.labels.template.disabled = true;
-    pieSeries.ticks.template.disabled = true;
+      // Add and configure Series
+      var pieSeries = x.series.push(new am4charts.PieSeries());
+      pieSeries.dataFields.value = "IKT";
+      pieSeries.dataFields.category = "Nutzen";
+      pieSeries.slices.template.strokeWidth = 0;
+      pieSeries.slices.template.tooltipText = "IKT: {IKT}";
+      pieSeries.radius = am4core.percent(43);
+      pieSeries.colors.list = [am4core.color("#EDE750"), am4core.color("rgba(0, 77, 136, 0.3)")];
 
-    // Disable sliding out of slices
-    pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
-    pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
-    // Add second series
-    var pieSeries2 = chart.series.push(new am4charts.PieSeries());
-    pieSeries2.dataFields.value = "Gesamtwirtschaft";
-    pieSeries2.dataFields.category = "Nutzen";
-    pieSeries2.slices.template.strokeWidth = 0;
-    pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
-    pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
-    pieSeries2.labels.template.disabled = true;
-    pieSeries2.ticks.template.disabled = true;
-    pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
-    pieSeries2.colors.list = [am4core.color("#4F4FFE"), am4core.color("rgba(0, 77, 136, 0.3)")];
+      // Disabling labels and ticks on inner circle
+      pieSeries.labels.template.disabled = true;
+      pieSeries.ticks.template.disabled = true;
 
-    let label = chart.createChild(am4core.Label);
-    label.text = "Maschinelles Lernen und Beweisen";
-    label.align = "center";
+      // Disable sliding out of slices
+      pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
+      // Add second series
+      var pieSeries2 = x.series.push(new am4charts.PieSeries());
+      pieSeries2.dataFields.value = "Gesamtwirtschaft";
+      pieSeries2.dataFields.category = "Nutzen";
+      pieSeries2.slices.template.strokeWidth = 0;
+      pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
+      pieSeries2.labels.template.disabled = true;
+      pieSeries2.ticks.template.disabled = true;
+      pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
+      pieSeries2.colors.list = [am4core.color("#4F4FFE"), am4core.color("rgba(0, 77, 136, 0.3)")];
 
-    let bullet = pieSeries.createChild(am4charts.Bullet);
-    let image = bullet.createChild(am4core.Image);
-    image.href = buchHref;
-    image.width = 50;
-    image.height = 50;
-    image.horizontalCenter = "middle";
-    image.verticalCenter = "middle";
+      let label = x.createChild(am4core.Label);
+      label.text = "Maschinelles Lernen und Beweisen";
+      label.align = "center";
 
-    chart.logo.disabled = "true";
-  }); // end am4core.ready()
-  return <div id='chartdiv_sec6' style={{ width: "100%", height: "300px" }}></div>;
+      let bullet = pieSeries.createChild(am4charts.Bullet);
+      let image = bullet.createChild(am4core.Image);
+      image.href = buchHref;
+      image.width = 50;
+      image.height = 50;
+      image.horizontalCenter = "middle";
+      image.verticalCenter = "middle";
+
+      x.logo.disabled = true;
+      chart.current = x;
+    }
+  }, [inViewport]); // end am4core.ready()
+  return (
+    <div id='chartdiv_sec6' style={{ width: "100%", height: "300px" }} ref={forwardedRef}></div>
+  );
 }
 
-export default Chart1;
+export default handleViewport(Chart1);
--- src/sections/section7/Chart2.jsx
+++ src/sections/section7/Chart2.jsx
@@ -3,76 +3,86 @@ import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import am4themes_wdTheme from "../../theme";
 import { cycleLoadingHref } from "../../svg/svgHref";
+import handleViewport from "react-in-viewport";
+import { useLayoutEffect, useRef } from "react";
 
 am4core.useTheme(am4themes_animated);
 am4core.useTheme(am4themes_wdTheme);
 
-function Chart2() {
-  am4core.ready(function () {
-    // Create chart instance
-    var chart = am4core.create("chartdiv_sec7_2", am4charts.PieChart);
+function Chart2(props) {
+  const chart = useRef(null);
+  const { inViewport, forwardedRef } = props;
 
-    // Let's cut a hole in our Pie chart the size of 40% the radius
-    chart.innerRadius = am4core.percent(40);
+  useLayoutEffect(() => {
+    if (inViewport) {
+      // Create chart instance
+      var x = am4core.create("chartdiv_sec7_2", am4charts.PieChart);
 
-    // Add data
-    chart.data = [
-      {
-        Nutzen: "Nutzen Automatisierung von Prozessen",
-        IKT: 54,
-        Gesamtwirtschaft: 56,
-      },
-      {
-        Nutzen: "Nutzen es nicht",
-        IKT: 46,
-        Gesamtwirtschaft: 44,
-      },
-    ];
+      // Let's cut a hole in our Pie chart the size of 40% the radius
+      x.innerRadius = am4core.percent(40);
 
-    // Add and configure Series
-    var pieSeries = chart.series.push(new am4charts.PieSeries());
-    pieSeries.dataFields.value = "IKT";
-    pieSeries.dataFields.category = "Nutzen";
-    pieSeries.slices.template.strokeWidth = 0;
-    pieSeries.slices.template.tooltipText = "IKT: {IKT}";
-    pieSeries.radius = am4core.percent(43);
-    pieSeries.colors.list = [am4core.color("#EDE750"), am4core.color("rgba(0, 77, 136, 0.3)")];
+      // Add data
+      x.data = [
+        {
+          Nutzen: "Nutzen Automatisierung von Prozessen",
+          IKT: 54,
+          Gesamtwirtschaft: 56,
+        },
+        {
+          Nutzen: "Nutzen es nicht",
+          IKT: 46,
+          Gesamtwirtschaft: 44,
+        },
+      ];
 
-    // Disabling labels and ticks on inner circle
-    pieSeries.labels.template.disabled = true;
-    pieSeries.ticks.template.disabled = true;
+      // Add and configure Series
+      var pieSeries = x.series.push(new am4charts.PieSeries());
+      pieSeries.dataFields.value = "IKT";
+      pieSeries.dataFields.category = "Nutzen";
+      pieSeries.slices.template.strokeWidth = 0;
+      pieSeries.slices.template.tooltipText = "IKT: {IKT}";
+      pieSeries.radius = am4core.percent(43);
+      pieSeries.colors.list = [am4core.color("#EDE750"), am4core.color("rgba(0, 77, 136, 0.3)")];
 
-    // Disable sliding out of slices
-    pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
-    pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
+      // Disabling labels and ticks on inner circle
+      pieSeries.labels.template.disabled = true;
+      pieSeries.ticks.template.disabled = true;
 
-    // Add second series
-    var pieSeries2 = chart.series.push(new am4charts.PieSeries());
-    pieSeries2.dataFields.value = "Gesamtwirtschaft";
-    pieSeries2.dataFields.category = "Nutzen";
-    pieSeries2.slices.template.strokeWidth = 0;
-    pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
-    pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
-    pieSeries2.labels.template.disabled = true;
-    pieSeries2.ticks.template.disabled = true;
-    pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
-    pieSeries2.colors.list = [am4core.color("#4F4FFE"), am4core.color("rgba(0, 77, 136, 0.3)")];
+      // Disable sliding out of slices
+      pieSeries.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries.slices.template.states.getKey("hover").properties.scale = 0.9;
 
-    let label = chart.createChild(am4core.Label);
-    label.text = "Automatisierung von Prozessen";
-    label.align = "center";
+      // Add second series
+      var pieSeries2 = x.series.push(new am4charts.PieSeries());
+      pieSeries2.dataFields.value = "Gesamtwirtschaft";
+      pieSeries2.dataFields.category = "Nutzen";
+      pieSeries2.slices.template.strokeWidth = 0;
+      pieSeries2.slices.template.states.getKey("hover").properties.shiftRadius = 0;
+      pieSeries2.slices.template.states.getKey("hover").properties.scale = 1.1;
+      pieSeries2.labels.template.disabled = true;
+      pieSeries2.ticks.template.disabled = true;
+      pieSeries2.slices.template.tooltipText = "Gesamtwirtschaft: {Gesamtwirtschaft}";
+      pieSeries2.colors.list = [am4core.color("#4F4FFE"), am4core.color("rgba(0, 77, 136, 0.3)")];
 
-    let bullet = pieSeries.createChild(am4charts.Bullet);
-    let image = bullet.createChild(am4core.Image);
-    image.href = cycleLoadingHref;
-    image.width = 50;
-    image.height = 50;
-    image.horizontalCenter = "middle";
-    image.verticalCenter = "middle";
+      let label = x.createChild(am4core.Label);
+      label.text = "Automatisierung von Prozessen";
+      label.align = "center";
 
-    chart.logo.disabled = "true";
-  }); // end am4core.ready()
-  return <div id="chartdiv_sec7_2" style={{ width: "100%", height: "300px" }}></div>;
+      let bullet = pieSeries.createChild(am4charts.Bullet);
+      let image = bullet.createChild(am4core.Image);
+      image.href = cycleLoadingHref;
+      image.width = 50;
+      image.height = 50;
+      image.horizontalCenter = "middle";
+      image.verticalCenter = "middle";
+
+      x.logo.disabled = true;
+      chart.current = x;
+    }
+  }, [inViewport]);
+  return (
+    <div id='chartdiv_sec7_2' style={{ width: "100%", height: "300px" }} ref={forwardedRef}></div>
+  );
 }
 
-export default Chart2;
+export default handleViewport(Chart2);
