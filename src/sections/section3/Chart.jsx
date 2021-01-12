224cbad38512d5c946af7d8a4cadbe748adf445c
eisenils <mail.eisenils@gmail.com>
2021-01-12 08:34:16 +0100

    Change theme colors and optimize Section 3 ☃

--- src/sections/section3/Chart.jsx
+++ src/sections/section3/Chart.jsx
@@ -28,104 +28,106 @@ function Chart(props) {
     if (inViewport) {
       var x = am4core.create("stackedbarchart", am4charts.XYChart);
 
-    x.paddingLeft = 50;
-    x.maskBullets = false;
+      x.paddingLeft = 60;
+      x.maskBullets = false;
 
-    // Add data
-    x.data = [
-      {
-        branch: "Gesamtwirtschaft",
-        essenziell: 12,
-        wichtig: 65,
-        wenigerwichtig: 23,
-        icon: Gesamtwirtschaft,
-      },
-      {
-        branch: "Finanzdienstleist.",
-        wichtig: 68,
-        wenigerwichtig: 32,
-        icon: Finanzdienstleist,
-      },
-      {
-        branch: "Fahrzeugbau",
-        essenziell: 4.4,
-        wichtig: 71.2,
-        wenigerwichtig: 24.4,
-        icon: Fahrzeugbau,
-      },
-      {
-        branch: "Verkehr, Logistik",
-        essenziell: 10.2,
-        wichtig: 72.8,
-        wenigerwichtig: 17,
-        icon: VerkehrLogistik,
-      },
-      {
-        branch: "Untern.nahe Dienstl.",
-        essenziell: 10.2,
-        wichtig: 66.4,
-        wenigerwichtig: 23.4,
-        icon: UnternNaheDiesnt,
-      },
-      {
-        branch: "Ver-/Entsorg., Bg.b.",
-        essenziell: 10.2,
-        wichtig: 72.4,
-        wenigerwichtig: 17.4,
-        icon: Entsorg,
-      },
-      {
-        branch: "Chemie/Ph., Gr.st.",
-        essenziell: 11,
-        wichtig: 74,
-        wenigerwichtig: 15,
-        icon: Chemie,
-      },
-      {
-        branch: "Elektrot./Maschin.b.",
-        essenziell: 12,
-        wichtig: 72,
-        wenigerwichtig: 16,
-        icon: Maschinenbau,
-      },
-      {
-        branch: "Sonst. Verarb. Gew.",
-        essenziell: 13,
-        wichtig: 60,
-        wenigerwichtig: 27,
-        icon: SonstVerarb,
-      },
-      {
-        branch: "IKT",
-        essenziell: 17,
-        wichtig: 60,
-        wenigerwichtig: 23,
-        icon: IKT,
-      },
-      {
-        branch: "Großhandel",
-        essenziell: 17,
-        wichtig: 64,
-        wenigerwichtig: 19,
-        icon: Großhandel,
-      },
-      {
-        branch: "Sonst. Dienstleist.",
-        essenziell: 20,
-        wichtig: 56,
-        wenigerwichtig: 24,
-        icon: SonstDienst,
-      },
-    ];
+      // Add data
+      x.data = [
+        {
+          branch: "Gesamtwirtschaft",
+          essenziell: 12,
+          wichtig: 65,
+          wenigerwichtig: 23,
+          icon: Gesamtwirtschaft,
+        },
+        {
+          branch: "Finanzdienstleist.",
+          wichtig: 68,
+          wenigerwichtig: 32,
+          icon: Finanzdienstleist,
+        },
+        {
+          branch: "Fahrzeugbau",
+          essenziell: 4.4,
+          wichtig: 71.2,
+          wenigerwichtig: 24.4,
+          icon: Fahrzeugbau,
+        },
+        {
+          branch: "Verkehr, Logistik",
+          essenziell: 10.2,
+          wichtig: 72.8,
+          wenigerwichtig: 17,
+          icon: VerkehrLogistik,
+        },
+        {
+          branch: "Untern.nahe Dienstl.",
+          essenziell: 10.2,
+          wichtig: 66.4,
+          wenigerwichtig: 23.4,
+          icon: UnternNaheDiesnt,
+        },
+        {
+          branch: "Ver-/Entsorg., Bgergb.",
+          essenziell: 10.2,
+          wichtig: 72.4,
+          wenigerwichtig: 17.4,
+          icon: Entsorg,
+        },
+        {
+          branch: "Chemie/Ph., Grundst. In.",
+          essenziell: 11,
+          wichtig: 74,
+          wenigerwichtig: 15,
+          icon: Chemie,
+        },
+        {
+          branch: "Elektrot./Maschin.b.",
+          essenziell: 12,
+          wichtig: 72,
+          wenigerwichtig: 16,
+          icon: Maschinenbau,
+        },
+        {
+          branch: "Sonst. Verarb. Gew.",
+          essenziell: 13,
+          wichtig: 60,
+          wenigerwichtig: 27,
+          icon: SonstVerarb,
+        },
+        {
+          branch: "IKT",
+          essenziell: 17,
+          wichtig: 60,
+          wenigerwichtig: 23,
+          icon: IKT,
+        },
+        {
+          branch: "Großhandel",
+          essenziell: 17,
+          wichtig: 64,
+          wenigerwichtig: 19,
+          icon: Großhandel,
+        },
+        {
+          branch: "Sonst. Dienstleist.",
+          essenziell: 20,
+          wichtig: 56,
+          wenigerwichtig: 24,
+          icon: SonstDienst,
+        },
+      ];
 
       x.legend = new am4charts.Legend();
       x.legend.position = "bottom";
+      x.legend.dx = 11;
+      x.legend.dy = 20;
 
-    // Create axes
-    let categoryAxis = x.yAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "branch";
-    categoryAxis.renderer.grid.template.opacity = 0;
-    categoryAxis.renderer.labels.template.dx = -50;
+      // Create axes
+      let categoryAxis = x.yAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "branch";
+      categoryAxis.renderer.grid.template.opacity = 0;
+      categoryAxis.renderer.labels.template.dx = -70;
 
       let valueAxis = x.xAxes.push(new am4charts.ValueAxis());
       valueAxis.min = 0;
@@ -158,21 +160,21 @@ function Chart(props) {
       x.language.locale["_decimalSeparator"] = ",";
       x.logo.disabled = true;
 
-    var image = new am4core.Image();
-    image.width = 35;
-    image.height = 35;
-    image.verticalCenter = "middle";
-    image.horizontalCenter = "right";
-    image.dx = -10;
-    image.adapter.add("href", (href, target) => {
-      if (target.dataItem) {
-        return target.dataItem._dataContext.icon;
-      }
-      return href;
-    });
-    categoryAxis.dataItems.template.bullet = image;
+      var image = new am4core.Image();
+      image.width = 35;
+      image.height = 35;
+      image.verticalCenter = "middle";
+      image.horizontalCenter = "right";
+      image.dx = -25;
+      image.adapter.add("href", (href, target) => {
+        if (target.dataItem) {
+          return target.dataItem._dataContext.icon;
+        }
+        return href;
+      });
+      categoryAxis.dataItems.template.bullet = image;
 
-    chart.current = x;
+      chart.current = x;
 
       return () => {
         x.dispose();
@@ -181,7 +183,7 @@ function Chart(props) {
   }, [inViewport]);
 
   return (
-    <div id='stackedbarchart' style={{ width: "100%", height: "600px" }} ref={forwardedRef}></div>
+    <div id="stackedbarchart" style={{ width: "100%", height: "600px" }} ref={forwardedRef}></div>
   );
 }
 
--- src/theme.js
+++ src/theme.js
@@ -8,7 +8,7 @@ function am4themes_wdTheme(target) {
     target.fontFamily = "IBM Plex Sans";
   }
   if (target instanceof am4core.ColorSet) {
-    target.list = [am4core.color("#11A6B6"), am4core.color("#227AA4"), am4core.color("#004D88")];
+    target.list = [am4core.color("#65737E"), am4core.color("#343D46"), am4core.color("#4F5B66")];
   }
 }
 
